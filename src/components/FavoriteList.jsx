5756b2fbe42e8d97767733406a477c9a520195c3
Nestor162 <nestorcicardini@gmail.com>
2023-04-07 20:26:04 +0200

    Stile cards

--- src/components/FavoriteList.jsx
+++ src/components/FavoriteList.jsx
@@ -1,5 +1,6 @@
-import { Alert, Col } from "react-bootstrap";
+import { Alert, Card, Col } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const FavoriteList = () => {
   const userFavorites = useSelector(state => state.favorites);
@@ -9,8 +10,22 @@ const FavoriteList = () => {
     <Col xs={9} md={7} className="mx-auto mt-5">
       {userFavorites.length > 0 ? (
         <>
-          {userFavorites.map(el => {
-            return <div>{el.name}</div>;
+          {userFavorites.map((el, i) => {
+            return (
+              <Card bg={"light"} key={i} text={"dark"} className="mb-4">
+                <Card.Header className="fs-3">
+                  <Link className="text-dark" style={{ textDecoration: "none" }} to={`/city/${el.name}`}>
+                    {el.name}
+                  </Link>
+                </Card.Header>
+                <Card.Body>
+                  <Card.Title className="fs-4"> {el.weather[0].main}</Card.Title>
+                  <Card.Text>
+                    <div className="fw-bold fs-2">{el.main.temp}°</div>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            );
           })}
         </>
       ) : (
