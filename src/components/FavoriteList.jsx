582f167b423b71063984fbf84e2761492db7d477
Nestor162 <nestorcicardini@gmail.com>
2023-04-07 20:59:43 +0200

    Stile card citta preferite

--- src/components/FavoriteList.jsx
+++ src/components/FavoriteList.jsx
@@ -1,4 +1,5 @@
 import { Alert, Card, Col } from "react-bootstrap";
+import { FiMinusCircle } from "react-icons/fi";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -13,10 +14,12 @@ const FavoriteList = () => {
           {userFavorites.map((el, i) => {
             return (
               <Card bg={"light"} key={i} text={"dark"} className="mb-4">
-                <Card.Header className="fs-3">
-                  <Link className="text-dark" style={{ textDecoration: "none" }} to={`/city/${el.name}`}>
-                    {el.name}
+                <Card.Header className="fs-3 d-flex justify-content-between align-items-center">
+                  <Link className="text-dark" to={`/city/${el.name}`}>
+                    <span className="cityNameCard">{el.name}</span>{" "}
+                    <span className="text-secondary fs-4">({el.sys.country})</span>
                   </Link>
+                  <FiMinusCircle className="navIcons removeIcon" />
                 </Card.Header>
                 <Card.Body>
                   <Card.Title className="fs-4"> {el.weather[0].main}</Card.Title>
