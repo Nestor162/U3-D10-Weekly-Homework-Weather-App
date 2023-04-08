d3e7e98979923a9e6ee4cafc18347d6a2236be36
Nestor162 <nestorcicardini@gmail.com>
2023-04-08 12:40:28 +0200

    Fix bug fetch nella citta selezionata

--- src/components/FavoriteList.jsx
+++ src/components/FavoriteList.jsx
@@ -1,14 +1,40 @@
 import { Alert, Card, Col, Container } from "react-bootstrap";
 import { FiMinusCircle } from "react-icons/fi";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import WelcomeMessage from "./WelcomeMessage";
+import { useEffect, useState } from "react";
 
 const FavoriteList = () => {
   const userFavorites = useSelector(state => state.favorites);
-  console.log(userFavorites);
+  const [geocoding, setGeocoding] = useState("");
+  const [selectedCity, setSelectedCity] = useState(null);
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchCordinates = city => {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=c0ec525b97319fc8a90fcad3f3ee5991`)
+      .then(response => response.json())
+      .then(data => {
+        setGeocoding(data);
+      });
+  };
+  const handleClick = city => {
+    setSelectedCity(city);
+
+    fetchCordinates(city);
+  };
+
+  useEffect(() => {
+    if (geocoding) {
+      console.log("selected:" + selectedCity);
+      dispatch({ type: "GET_COORDINATES", payload: [geocoding[0].lat, geocoding[0].lon] });
+      navigate(`/city/${selectedCity}`);
+      console.log("cambiamento abilitato");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [geocoding]);
 
   return (
     <Col xs={9} md={7} className="mx-auto mt-5">
@@ -18,7 +44,7 @@ const FavoriteList = () => {
             return (
               <Card bg={"light"} key={i} text={"dark"} className="mb-4">
                 <Card.Header className="fs-3 d-flex justify-content-between align-items-center">
-                  <Link className="text-dark" to={`/city/${el.name}`}>
+                  <Link className="text-dark" onClick={() => handleClick(el.name)}>
                     <span className="cityNameCard">{el.name}</span>{" "}
                     <span className="text-secondary fs-4">({el.sys.country})</span>
                   </Link>
