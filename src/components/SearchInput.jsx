1e395b96e1a20f62212cda1fd3f2d3e474dd5a29
Nestor162 <nestorcicardini@gmail.com>
2023-04-10 16:33:14 +0200

    Gestione errori

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,4 +1,4 @@
-import { Col, Form, Row, Spinner } from "react-bootstrap";
+import { Alert, Col, Form, Row, Spinner } from "react-bootstrap";
 import "../App.css";
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -19,12 +19,40 @@ const SearchInput = () => {
   // Uso lo hook useRef per 'selezionare' l'imput e successivamente dargli il focus
   const inputRef = useRef();
 
+  const [error, setError] = useState(false);
   const fetchCordinates = () => {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=c0ec525b97319fc8a90fcad3f3ee5991`)
-      .then(response => response.json())
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          switch (response.status) {
+            case 400:
+              throw new Error("Bad Request: The request could not be understood or was missing required parameters.");
+            case 401:
+              throw new Error("Unauthorized: The API key provided is not valid or has expired.");
+            case 403:
+              throw new Error("Forbidden: The server understood the request, but is refusing to fulfill it.");
+            case 404:
+              throw new Error("Not Found: The requested resource could not be found on the server.");
+            case 429:
+              throw new Error("Too Many Requests: The user has sent too many requests in a given amount of time.");
+            case 500:
+              throw new Error(
+                "Internal Server Error: The server encountered an unexpected condition that prevented it from fulfilling the request."
+              );
+            default:
+              throw new Error("Something went wrong with the fetch call");
+          }
+        }
+      })
       .then(data => {
         setGeocoding(data);
         setLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setError(error);
       });
   };
 
@@ -56,6 +84,11 @@ const SearchInput = () => {
           <img src={icon} alt="app icon" className="d-inline-block" style={{ width: "70px" }} />
         </Col>
       </Row>
+      {error && (
+        <Alert variant="danger" className="position-absolute top-0 end-0 start-0">
+          ⚠️ Error: {error.message}
+        </Alert>
+      )}
       {loading && (
         <Spinner
           className="position-absolute"
